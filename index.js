d1ab15a7626fe135699342c075fabe4598cf15f7
LDeffinis <louis.deffinis@gmail.com>
2021-03-01 15:34:30 +0100

    ajout de popup et vérification des passwords

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 $(function() {
 
 $( "#signupBtn" ).click(function() {
-    console.log('click');
+
+    let p1 = $('#password').val();
+    let p2 = $('#confirmPassword').val();
+
+    if(p1==p2){
+
+        $('#infoPopUpText').html('Enregistrement réussi');
+
+        $('#username').val('');
+        $('#password').val('');
+        $('#confirmPassword').val('');
+    }
+    else{
+
+        $('#infoPopUpText').html('Les mots de passe ne correspondent pas');
+
+        $('#password').val('');
+        $('#confirmPassword').val('');
+    }
   });
 
 });
